7965607f5527a78dc3fe344ab05fcae58c08bfed
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2025-05-24 21:59:07 -0500

    refactor: replace NotFoundException with RpcException in ProductsService

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,5 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -60,7 +56,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException({
+        message: `Product with id ${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
     }
 
     return product;
